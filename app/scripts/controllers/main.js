df40e304a7d0fcf3bfec4341327ef04c7ee97a29
Anartz Mugika <mugan86@gmail.com>
2016-07-19 07:40:57 +0200

    Update form layout and add submit function to manage form info

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -27,5 +27,14 @@ angular.module('angularApp')
     type: {id: '4', name: 'All users'} //This sets the default value of the select in the ui
     };
 
-    
+    $scope.htmlmsg = "";
+    $scope.image = "";
+
+    $scope.sendPush = function()
+    {
+    	console.log("Send message");
+    	console.log($scope.image);
+    	console.log($scope.htmlmsg);
+    	console.log($scope.data.type.id);
+    };
   });
