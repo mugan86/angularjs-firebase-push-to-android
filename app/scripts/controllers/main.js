4bfbc7988839e81b39248cdbdf82032a7bb2ace7
Anartz Mugika <mugan86@gmail.com>
2016-07-19 07:07:25 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('MainCtrl', function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+
+    $scope.data = {
+    availableOptions: [
+
+      {id: '1', name: 'Sports'},
+      {id: '2', name: 'Economy'},
+      {id: '3', name: 'Technology'},
+      {id: '4', name: 'All users'}
+    ],
+    type: {id: '4', name: 'All users'} //This sets the default value of the select in the ui
+    };
+
+    
+  });
