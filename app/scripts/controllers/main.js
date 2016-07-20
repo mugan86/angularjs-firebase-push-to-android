37869256d53bb264ebffc818de0e86eaa523f8ba
Anartz Mugika <mugan86@gmail.com>
2016-07-20 11:51:07 +0200

    Manage Push messages, to add/remove (limit 5 and min 1) and add function to send server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,46 +8,118 @@
  * Controller of the angularApp
  */
 angular.module('angularApp')
-  .controller('MainCtrl', function ($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    
+    //Initialize add/remove messages buttons
+    $scope.remove = false;
+    $scope.add = true;
+
+    $scope.description = "";
 
     $scope.addMessage = function()
     {
-      console.log("Add new messa");
+      $scope.choices.push({id: $scope.choices.length+1, image: "", htmlmsg: ""});
+      $scope.remove = true;
+      if ($scope.choices.length == 5)
+      {
+        $scope.add = false;
+      }
+    };
+
+    $scope.removeMessage = function() {
+      if ($scope.choices.length > 1)
+      {
+        var lastItem = $scope.choices.length-1;
+        $scope.choices.splice(lastItem);
+        if ($scope.choices.length == 1)
+        {
+          $scope.remove = false;
+        }
+        else
+        {
+          if ($scope.choices.length < 5)
+          {
+            $scope.add = true;
+          }
+          $scope.remove = true;
+        } 
+      }
     };
 
     $scope.choices = [{id: 1, image: "", htmlmsg: ""}];
-    $scope.data = {
-    availableOptions: [
-
-      {id: '1', name: 'Sports'},
-      {id: '2', name: 'Economy'},
-      {id: '3', name: 'Technology'},
-      {id: '4', name: 'All users'}
-    ],
-    type: {id: '4', name: 'All users'} //This sets the default value of the select in the ui
-    };
+    
 
     $scope.htmlmsg = "";
     $scope.image = "";
 
+    $scope.startSelectValues = function()
+    {
+        $scope.data = {
+        availableOptions: [
+
+            {id: '1', name: 'Sports'},
+            {id: '2', name: 'Economy'},
+            {id: '3', name: 'Technology'},
+            {id: '4', name: 'All users'}
+          ],
+          type: {id: '4', name: 'All users'} //This sets the default value of the select in the ui
+          };
+    };
+
     $scope.sendPush = function(form)
     {
       console.log("Send message");
-      if(form.$valid) {
-        if ($scope.form.image[0] != undefined)
-        {
-          console.log($scope.form.image[0]);
-        }
-        
-        console.log($scope.form.htmlmsg[0]);
-        console.log($scope.data.type.id);
+
+      for (var i = 0; i < $scope.choices.length; i++)
+      {
+        console.log($scope.choices[i].id + " / " + $scope.choices[i].image + " / " + $scope.choices[i].htmlmsg);
       }
-    	
+
+      console.log($scope.data.type.id);
+      console.log($scope.description);
+      //$scope.content = 
+      console.log(angular.toJson($scope.choices));
+
+      $scope.formData = {
+        'type': $scope.data.type.id,
+        'description': $scope.description,
+        'messages': $scope.choices
+      };
+      console.log(angular.toJson($scope.formData));
+
+      console.log({user:$scope.formData});
+      /*$http.post('http://192.168.110.131:8888/push/receive_data.php', {data:$scope.formData}).success(
+        function(data){
+          $scope.response = data;
+          console.log($scope.response);
+      }).error(function()
+          {
+            console.log('Error');
+             
+          })
+          .then(function() {
+
+            console.log("correct");
+
+        });*/
+
+        var data = $.param({'type': $scope.data.type.id,
+                            'description': $scope.description,
+                            'messages': angular.toJson($scope.choices),
+                            'test': "ddddd"});        
+        var config = {headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}} 
+
+        $http.post("http://192.168.110.131:8888/push/receive_data.php",data,config)
+        .success(function(response) {
+            $scope.data = response;
+            console.log($scope.data);
+            $scope.startSelectValues();
+        });
+
+
+
       
     };
+
+    $scope.startSelectValues();
   });
