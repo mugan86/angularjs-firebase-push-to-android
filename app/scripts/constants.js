0092100d3d77af57ffe95c56719bad6e5dd035cc
Anartz Mugika <mugan86@gmail.com>
2016-07-27 10:53:42 +0200

    Add Constants file

---
+++ app/scripts/constants.js
@@ -0,0 +1,22 @@
+/**********************************************************************************************
+Constant values files
+----------------------------
+
+
+***********************************************************************************************/
+angular.module('angularApp')
+
+.constant('LOCAL_PUSH_SERVER_URL_LOCALHOST', 'http://192.168.110.131/push/receive_data.php')
+.constant('WIKIPEDIA_MOBILE_WIKI_URL', 'https://en.m.wikipedia.org/wiki/')
+.constant('BASKETBALL_REFERENCE', 'http://www.basketball-reference.com')
+.constant('F1', {
+                        main_url: 'http://ergast.com/api/f1/',
+                        drivers: 'drivers.json',
+                        circuits: 'circuits.json'
+                    })
+.constant('REST_COUNTRIES', {
+                        main_url: 'http://restcountries.eu',
+                        all: '/rest/v1/all'
+                    })
+.constant('MENDIKAT', 'http://www.mendikat.net/')
+;
